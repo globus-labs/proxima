8a185d1e6153fc2496ad61edb5b40a3321422429
Logan Ward <ward.logan.t@gmail.com>
2019-06-14 14:34:03 -0500

    Added training time to performance stats

--- proxima/decorator.py
+++ proxima/decorator.py
@@ -15,7 +15,7 @@ from warnings import warn
 # TODO (wardlt): Add preprocessing class for transforming function inputs
 # TODO (wardlt): Add statistics class
 
-_perf_info = namedtuple('AccelStats', ['lfa_runs', 'lfa_time', 'uq_time',
+_perf_info = namedtuple('AccelStats', ['lfa_runs', 'lfa_time', 'uq_time', 'train_time',
                                         'target_runs', 'target_time'])
 
 
@@ -34,13 +34,13 @@ def lfa(inference_engine: BaseInferenceEngine, uq_engine: BaseUQEngine,
         """Function that applies the decorator function"""
 
         # Initialize the storage for performance counters
-        lfa_runs = lfa_time = uq_time = target_runs = target_time = 0
+        lfa_runs = lfa_time = uq_time = target_runs = target_time = train_time = 0
 
         # Make the decorator
         def decorator(*args, **kwargs):
             """Wrapper function for the target function"""
 
-            nonlocal lfa_runs, lfa_time, uq_time, target_runs, target_time
+            nonlocal lfa_runs, lfa_time, uq_time, target_runs, target_time, train_time
 
             # Make inputs from the position args only
             # TODO (lw): Replace with better strategy
@@ -73,12 +73,16 @@ def lfa(inference_engine: BaseInferenceEngine, uq_engine: BaseUQEngine,
                 data_source.add_pair(inputs, outputs)
 
                 # Retrain the model
-                train_engine.request_update(inference_engine, data_source)
+                start_time = perf_counter()
+                try:
+                    train_engine.request_update(inference_engine, data_source)
+                finally:
+                    train_time += perf_counter() - start_time
                 return outputs
 
         # Attach the stats to the decorator
         def _get_perf_info():
-            return _perf_info(lfa_runs, lfa_time, uq_time, target_runs, target_time)
+            return _perf_info(lfa_runs, lfa_time, uq_time, train_time, target_runs, target_time)
         decorator.get_performance_info = _get_perf_info
 
         # Attach the engines
