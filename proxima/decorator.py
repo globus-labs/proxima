f5a3bea1be07fce7c360a78f12ca2c5fca8dbab9
Logan Ward <ward.logan.t@gmail.com>
2019-06-14 12:52:02 -0500

    Added hit stats to decorator

--- proxima/decorator.py
+++ proxima/decorator.py
@@ -7,18 +7,24 @@ from proxima.data import BaseDataSource
 from proxima.uq import BaseUQEngine
 
 from functools import update_wrapper
+from collections import namedtuple
+from time import perf_counter
 from warnings import warn
 
 # TODO (wardlt): Make training/uq policies configurable
 # TODO (wardlt): Add preprocessing class for transforming function inputs
+# TODO (wardlt): Add statistics class
+
+_perf_info = namedtuple('AccelStats', ['lfa_runs', 'lfa_time', 'uq_time',
+                                        'target_runs', 'target_time'])
 
 
-def lfa(learner: BaseInferenceEngine, uq_engine: BaseUQEngine,
+def lfa(inference_engine: BaseInferenceEngine, uq_engine: BaseUQEngine,
         data_source: BaseDataSource, train_engine: TrainingEngine):
     """Make a wrapper that augments a function with a learned fnuction accelerator
 
     Args:
-        learner (BaseEstimator): Link to the inference engine
+        inference_engine (BaseEstimator): Link to the inference engine
         uq_engine (BaseUQEngine): Link to the UQ engine
         data_source (BaseDataSource): Link to the data source
         train_engine (TrainingEngine): Link to the retraining engine
@@ -27,27 +33,59 @@ def lfa(learner: BaseInferenceEngine, uq_engine: BaseUQEngine,
     def decorating_function(target_function):
         """Function that applies the decorator function"""
 
+        # Initialize the storage for performance counters
+        lfa_runs = lfa_time = uq_time = target_runs = target_time = 0
+
+        # Make the decorator
         def decorator(*args, **kwargs):
             """Wrapper function for the target function"""
 
+            nonlocal lfa_runs, lfa_time, uq_time, target_runs, target_time
+
             # Make inputs from the position args only
+            # TODO (lw): Replace with better strategy
             if len(kwargs) > 0:
                 warn('Keyword arguments are currently being ignored')
-            inputs = args  # TODO (lw): Just concatenate
+            inputs = args
 
             # Execute the approximation
-            if uq_engine.is_supported(learner, data_source, inputs):
-                return learner.infer(inputs)
+            start_time = perf_counter()
+            is_supported = uq_engine.is_supported(inference_engine, data_source, inputs)
+            uq_time += perf_counter() - start_time
+
+            if is_supported:
+                start_time = perf_counter()
+                try:
+                    return inference_engine.infer(inputs)
+                finally:
+                    lfa_runs += 1
+                    lfa_time += perf_counter() - start_time
             else:
                 # Run the original function
-                outputs = target_function(*args, **kwargs)
+                try:
+                    start_time = perf_counter()
+                    outputs = target_function(*args, **kwargs)
+                finally:
+                    target_runs += 1
+                    target_time += perf_counter() - start_time
 
                 # Store the data
                 data_source.add_pair(inputs, outputs)
 
                 # Retrain the model
-                train_engine.request_update(learner, data_source)
+                train_engine.request_update(inference_engine, data_source)
                 return outputs
+
+        # Attach the stats to the decorator
+        def _get_perf_info():
+            return _perf_info(lfa_runs, lfa_time, uq_time, target_runs, target_time)
+        decorator.get_performance_info = _get_perf_info
+
+        # Attach the engines
+        decorator.inference_engine = inference_engine
+        decorator.train_engine = train_engine
+        decorator.uq_engine = uq_engine
+        decorator.data_source = data_source
 
         return update_wrapper(decorator, target_function)
 
