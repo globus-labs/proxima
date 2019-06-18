af72cd5a88939bd8b529cf513331561a3c3d658d
Ben Blaiszik <blaiszik@uchicago.edu>
2019-06-18 13:34:04 -0500

    pep8 fixes

--- test/test_sql.py
+++ test/test_sql.py
@@ -6,17 +6,18 @@ db_file = '__test__prox'
 sqlite_url = 'sqlite:///{db_file}'.format(db_file=db_file)
 sqlite_table = 'data'
 
+
 def test_sqlite():
     # Should this be broken into smaller tests?
     list_size = 100
     data = SQLDataStorage(sqlite_url, table=sqlite_table)
-    inputs = [float(i) for i in range(0,list_size)]
-    outputs = [float(i**3) for i in range(0,list_size)]
+    inputs = [float(i) for i in range(0, list_size)]
+    outputs = [float(i**3) for i in range(0, list_size)]
     data.add_pairs(inputs, outputs)
 
     # Check that data sizes line up
-    assert(len(data.inputs) == list_size )
-    assert(len(data.outputs) == list_size )
+    assert(len(data.inputs) == list_size)
+    assert(len(data.outputs) == list_size)
 
     # Check that data can be retrieved
     r = data.get_all_data()
@@ -24,7 +25,3 @@ def test_sqlite():
 
     # Add tear down of database (delete file directly?)
     os.remove('./{db_file}'.format(db_file=db_file))
-
-
-
-
