3b37dba2365979eb01483047a82542160f0c1523
Ben Blaiszik <blaiszik@uchicago.edu>
2019-06-18 11:01:47 -0500

    First pass at SQL adapter. Should support any SQLAlchemy connectors (e.g., sqlite, mysql, postgresql)

--- proxima/data.py
+++ proxima/data.py
@@ -2,7 +2,8 @@
 
 from typing import Tuple, List, Iterator
 from random import shuffle
-
+from sqlalchemy import create_engine
+import pandas as pd
 
 class BaseDataSource:
     """Abstract class for managing training data"""
@@ -67,3 +68,49 @@ class InMemoryDataStorage(BaseDataSource):
         for start in range(0, len(self.inputs), batch_size):
             batch_inds = indices[start:start + batch_size]
             yield [self.inputs[i] for i in batch_inds], [self.outputs[i] for i in batch_inds]
+
+class SQLDataStorage(BaseDataSource):
+    """Store input/output pairs in memory without any persistence mechanism"""
+
+    def __init__(self, url, table="data"):
+        # TODO (wardlt): Do we want to optimize for insertion or random access
+        self.inputs = list()
+        self.outputs = list()
+        self.url = url  # sqlalchemy.engine.Engine or sqlite3.Connection
+        self.table = table
+        self.engine = create_engine(url,
+                                    echo=False)
+
+    def add_pair(self, inputs, outputs):
+        # Send to SQL
+        df = pd.DataFrame({"inputs": [inputs], "outputs": [outputs]})
+        df.set_index('inputs', inplace=True)
+        df.to_sql(self.table, con=self.engine, if_exists="append")
+
+        self.inputs.append(inputs)
+        self.outputs.append(outputs)
+
+    def add_pairs(self, inputs, outputs):
+        # Send to SQL
+        df = pd.DataFrame({"inputs": inputs, "outputs": outputs})
+        df.set_index('inputs', inplace=True)
+        df.to_sql(self.table, con=self.engine, if_exists="append")
+
+        self.inputs.extend(inputs)
+        self.outputs.extend(outputs)
+
+    def get_all_data(self):
+        q = "SELECT * FROM {table}".format(table=self.table)
+        return self.engine.execute(q).fetchall()
+
+    def iterate_over_data(self, batch_size: int):
+        res = True
+        q = "SELECT * FROM {table}".format(table=self.table)
+        r = self.engine.execute(q)
+        while res:
+            res = r.fetchmany(batch_size)
+            yield res
+
+    def clear_cache(self):
+        self.inputs = []
+        self.outputs = []
